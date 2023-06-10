0fa87467df45ba85c486047f907d0c44e99940b8
Ranon Diniz <115851280+ranondiniz@users.noreply.github.com>
2023-06-09 21:35:27 -0300

    Arquivos para recuperar informações sobre insulina

---
+++ recuperar_informacoes/calc_weight_json.py
@@ -0,0 +1,30 @@
+import jsonFileHandler
+
+data = jsonFileHandler.readJsonFile('files/insulin.json')
+if data != "" :
+    bInsulin = data['molecules']['bInsulin']
+    aInsulin = data['molecules']['aInsulin']
+    insulin = bInsulin + aInsulin
+    molecularWeightInsulinActual = data['molecularWeightInsulinActual']
+    print('bInsulin: ' + bInsulin)
+    print('aInsulin: ' + aInsulin)
+    print('molecularWeightInsulinActual: ' + str(molecularWeightInsulinActual))
+    
+    # Calculando o peso molecular da insulina
+    # Obtendo uma lista dos pesos dos aminoácidos (AA)  
+    
+    aaWeights = data['weights']
+    
+    # Conte o número de cada aminoácido  
+    
+    aaCountInsulin = ({x: float(insulin.upper().count(x)) for x in ['A','C','D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R','S', 'T','V', 'W', 'Y']})  
+   
+    # Multiplique a contagem pelos pesos  
+    
+    molecularWeightInsulin = sum({x: (aaCountInsulin[x]*aaWeights[x]) for x in
+    ['A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P', 'Q', 'R','S', 'T', 'V', 'W', 'Y']}.values())  
+    print("The rough molecular weight of insulin: " +
+    str(molecularWeightInsulin))
+    print("Percent error: " + str(((molecularWeightInsulin - molecularWeightInsulinActual)/molecularWeightInsulinActual)*100))
+else:
+    print("Error. Exiting program")
